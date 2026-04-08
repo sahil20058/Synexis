38b97addb614a150b6145d17687500a440d0cba9
Sahil Khan <168674941+sahil20058@users.noreply.github.com>
2026-04-08 22:13:11 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -40,8 +40,7 @@ def score():
     }
 
 def main():
-    # Run the FastAPI server
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860, reload=True)
 
 if __name__ == "__main__":
     main()
